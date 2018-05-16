238569bedcdea7b4c3e18b285ef3953a1060db2d
Bruce Chung <brucekchung@gmail.com>
2018-05-16 14:55:07 -0600

    Events render in calander - overlap and can extend multiple days

--- src/mockEvents.js
+++ src/mockEvents.js
@@ -1,7 +1,8 @@
 export const events = [
-  { name: 'event1', start: '5/15/2018', end: '5/15/2018' },
-  { name: 'event2', start: '5/18/2018', end: '5/20/2018' },
-  { name: 'event3', start: '5/23/2018', end: '5/23/2018' },
-  { name: 'event4', start: '5/26/2018', end: '5/27/2018' },
-  { name: 'event5', start: '6/02/2018', end: '6/02/2018' },
+  { name: 'workout', start: '5/15/2018', end: '5/15/2018' },
+  { name: 'siesta', start: '5/18/2018', end: '5/20/2018' },
+  { name: 'chipotle', start: '5/18/2018', end: '5/19/2018' },
+  { name: 'friends house', start: '5/23/2018', end: '5/23/2018' },
+  { name: 'code', start: '5/26/2018', end: '5/27/2018' },
+  { name: 'birthday', start: '6/02/2018', end: '6/02/2018' },
 ]
