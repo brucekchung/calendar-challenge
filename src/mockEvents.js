4635be34c47486fea1b9318b664d162768e498d0
Bruce Chung <brucekchung@gmail.com>
2018-05-16 14:33:40 -0600

    Add function to check if events match day in question

--- src/mockEvents.js
+++ src/mockEvents.js
@@ -1,6 +1,7 @@
 export const events = [
-  { start: '5/15/2018', end: '5/15/2018' },
-  { start: '5/18/2018', end: '5/18/2018' },
-  { start: '5/23/2018', end: '5/23/2018' },
-  { start: '5/26/2018', end: '5/26/2018' },
+  { name: 'event1', start: '5/15/2018', end: '5/15/2018' },
+  { name: 'event2', start: '5/18/2018', end: '5/20/2018' },
+  { name: 'event3', start: '5/23/2018', end: '5/23/2018' },
+  { name: 'event4', start: '5/26/2018', end: '5/27/2018' },
+  { name: 'event5', start: '6/02/2018', end: '6/02/2018' },
 ]
