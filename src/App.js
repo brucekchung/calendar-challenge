f63c5efde5dc6964d3f042218f69770312acedcf
Bruce Chung <brucekchung@gmail.com>
2018-05-16 10:21:20 -0600

    Add month and year to state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.allMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    this.state = {
+      currentMonth: '',
+      year: ''
+    }
+  }
+  
+  componentDidMount() {
+    const today = new Date() 
+    const month = this.allMonths[today.getMonth()]
+    const year = today.getYear()
+
+    this.setState({ 
+      currentMonth: month,
+      year
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        App
+        {console.log('month:', this.state.currentMonth)}
+        {console.log('year:', this.state.year)}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
