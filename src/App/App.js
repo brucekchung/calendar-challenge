e0741757ef50ea894469d1840062ff7b08c26901
Bruce Chung <brucekchung@gmail.com>
2018-05-16 11:15:52 -0600

    Calculate days in current and next month, minor refactors

--- src/App/App.js
+++ src/App/App.js
@@ -1,34 +1,49 @@
 import React, { Component } from 'react'
 import Header from '../Header/Header'
+import Calendar from '../Calendar/Calendar'
 import './App.css'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.allMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
     this.state = {
       currentMonth: '',
-      year: ''
+      nextMonth: '',
+      year: '',
+      daysInCurrentMonth: '',
+      daysInNextMonth: ''
     }
   }
+
+  //months arg for Date is 1-indexed...
+  daysInMonth = (year, month) => new Date(year, month + 1, 0).getDate()
   
   componentDidMount() {
+    const allMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     const today = new Date() 
-    const month = this.allMonths[today.getMonth()]
+    const monthNumber = today.getMonth()
+    const currentMonth = allMonths[monthNumber]
+    const nextMonth = allMonths[monthNumber + 1]
     const year = today.getFullYear()
+    const daysInCurrentMonth = this.daysInMonth(year, monthNumber) 
+    const daysInNextMonth = this.daysInMonth(year, monthNumber + 1)
 
     this.setState({ 
-      currentMonth: month,
-      year
+      currentMonth,
+      nextMonth,
+      year,
+      daysInCurrentMonth,
+      daysInNextMonth
     })
   }
 
   render() {
     return (
       <div className="App">
+        {console.log('info: ', this.state)}
         <Header date={ this.state } />
-        {console.log('month:', this.state.currentMonth)}
-        {console.log('year:', this.state.year)}
+        <Calendar />
       </div>
     )
   }
--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -1,1 +1,11 @@
+import React from 'react'
 
+const Calendar = () => {
+  return (
+    <div className="Calendar">
+      I'm a Calendar
+    </div>
+  )
+}
+
+export default Calendar
